3621191f7d3e007cd2207e6e6e29b1dee5485d60
Jonathan Jiang <jonathan.jiang01@sjsu.edu>
2025-07-12 00:30:23 +0800

    done the core part

--- main.cpp
+++ main.cpp
@@ -1,95 +1,181 @@
 #include <filesystem>
+
+// image
 #include <exiv2/exiv2.hpp>
-#include<iostream>
 #include <exiv2/error.hpp>
 
-using namespace std;
+// video
+#include "MediaInfo/MediaInfo.h"
 
-inline time_t file_time_to_time_t(std::filesystem::file_time_type ftime)
+#include <functional>
+#include <iostream>
+#include <algorithm>
+#include <chrono>
+#include <ctime>
+
+#include <locale>
+#include <codecvt>
+
+namespace fs = std::filesystem;
+
+inline MediaInfoLib::String make_mi_string(const std::string &s)
 {
-    using namespace std::chrono;
-    auto sctp = system_clock::now() + (ftime - decltype(ftime)::clock::now());
-
-    // Step 2: duration 可能是 common_type，要強制 cast
-    auto tp = time_point_cast<system_clock::duration>(sctp);
-
-    // to seconds
-    return system_clock::to_time_t(tp);
+    #ifdef UNICODE
+        return std::wstring(s.begin(), s.end());
+    #else
+        return s;
+    #endif
 }
 
-inline string time_t_to_date_string(time_t t) {
+inline std::string make_std_string(const MediaInfoLib::String &s)
+{
+    #ifdef UNICODE
+        std::wstring_convert<std::codecvt_utf8_utf16<wchar_t>> conv;
+        return conv.to_bytes(s);
+    #else
+        return s;
+    #endif
+}
+
+// Convert std::filesystem::file_time_type to time_t
+inline time_t file_time_to_time_t(fs::file_time_type ftime)
+{
+    auto raw = std::chrono::system_clock::now() + (ftime - decltype(ftime)::clock::now());
+    auto time_point = std::chrono::time_point_cast<std::chrono::system_clock::duration>(raw);
+    return std::chrono::system_clock::to_time_t(time_point);
+}
+
+// Format time_t to "YYYY-MM-DD"
+inline std::string time_t_to_date_string(time_t t)
+{
     tm *local_tm = localtime(&t);
     char buf[16];
-    std::strftime(buf, sizeof(buf), "%Y-%m-%d", local_tm);
+    strftime(buf, sizeof(buf), "%Y-%m-%d", local_tm);
     return std::string(buf);
 }
 
-int main() {
-    filesystem::path currentDir = filesystem::current_path();
-
-    for (const filesystem::directory_entry &currentEntry : filesystem::directory_iterator(currentDir))
-    {
-        cout << "reading from " << currentEntry.path().filename() << endl;
-        if (currentEntry.is_regular_file())
+inline bool move_to_file(const fs::path &from, fs::path to)
+{
+    try {
+        int counter = 1;
+        // in case we have images with the same name that is currently in the folder
+        while (fs::exists(to))
         {
-            filesystem::path filePath = currentEntry.path();
-            string extension = filePath.extension().string();
-            // C style tolower
-            transform(extension.begin(), extension.end(), extension.begin(), ::tolower);
-
-            // ================================
-            // 1) 圖片格式 => 用 EXIF
-            // ================================
-            if (extension == ".jpg" || extension == ".jpeg" || extension == ".png" || extension == ".heic")
-            {
-                try {
-                    unique_ptr<Exiv2::Image> image = Exiv2::ImageFactory::open(filePath.string());
-
-                    // 不先呼叫 readMetadata() → exifData() 裡面會是空的！
-                    image -> readMetadata();
-                    auto &exifData = image->exifData();
-
-                    Exiv2::ExifData::iterator it = exifData.findKey(Exiv2::ExifKey("Exif.Photo.DateTimeOriginal"));
-                    string datePart = "";
-                    if (it == exifData.end()) {
-                        std::filesystem::file_time_type ftime = filesystem::last_write_time(currentEntry);
-                        time_t timeSeconds = file_time_to_time_t(ftime);
-                        datePart = time_t_to_date_string(timeSeconds);
-                    } else {
-                        string extractedDate = it->value().toString();
-                        datePart = extractedDate.substr(0, 10);
-                        replace(datePart.begin(), datePart.end(), ':', '-');
-                    }
-
-                    filesystem::path targetDir = currentDir / datePart;
-                    if (filesystem::exists(targetDir))
-                    {
-                        cerr << "File already exists: " << targetDir << endl;
-                    } else {
-                        filesystem::create_directory(targetDir);
-                    }
-
-                    filesystem::path targetFile = targetDir / filePath.filename();
-                    int counter = 1;
-                    while (filesystem::exists(targetFile))
-                    {
-                        // stem() => file name without extension
-                        targetFile = targetDir / (filePath.stem().string() + "_" + to_string(counter) + filePath.extension().string());
-                        counter++;
-                    }
-                    filesystem::rename(filePath, targetFile);
-                    cout << "Moved " << filePath << " -> " << targetFile << endl;
-                } catch (const Exiv2::Error &e)
-                {
-                    std::cerr << "EXIF read failed: " << e.what() << std::endl;
-                }
-            }
-
-            // ================================
-            // 1) 影片格式 => 用 EXIF
-            // ================================
+            to = from.parent_path() /
+                 (from.stem().string() + "_" + std::to_string(counter) + from.extension().string());
+            ++counter;
         }
+        fs::rename(from, to);
     }
-    return 0;
+    catch (fs::filesystem_error &e)
+    {
+        std::cerr << "Move file failed: " << e.what() << "\n";
+        return false;
+    }
+    return true;
 }
 
+int main()
+{
+    fs::path current_dir = fs::current_path();
+
+    for (const auto &entry : fs::directory_iterator(current_dir))
+    {
+        std::cout << "Reading: " << entry.path().filename() << std::endl;
+
+        if (!entry.is_regular_file())
+        {
+            continue;
+        }
+
+        fs::path file_path = entry.path();
+        std::string extension = file_path.extension().string();
+        transform(extension.begin(), extension.end(), extension.begin(), ::tolower);
+
+        // ================================
+        // Handle image formats using EXIF
+        // ================================
+        if (extension == ".jpg" || extension == ".jpeg" || extension == ".png" || extension == ".heic")
+        {
+            try
+            {
+                std::unique_ptr<Exiv2::Image> image = Exiv2::ImageFactory::open(file_path.string());
+                image->readMetadata();
+                auto &exif_data = image->exifData();
+
+                auto it = exif_data.findKey(Exiv2::ExifKey("Exif.Photo.DateTimeOriginal"));
+                std::string date_part;
+
+                if (it == exif_data.end())
+                {
+                    // Note: this is not the correct picture taken time
+                    auto ftime = fs::last_write_time(entry);
+                    time_t time_seconds = file_time_to_time_t(ftime);
+                    date_part = time_t_to_date_string(time_seconds);
+                }
+                else
+                {
+                    std::string extracted_date = it->value().toString();
+                    date_part = extracted_date.substr(0, 10);
+                    replace(date_part.begin(), date_part.end(), ':', '-');
+                }
+
+                fs::path target_dir = current_dir / date_part;
+                if (!fs::exists(target_dir))
+                {
+                    fs::create_directory(target_dir);
+                }
+
+                fs::path target_file_path = target_dir / file_path.filename();
+
+                move_to_file(file_path, target_file_path);
+            }
+            catch (const Exiv2::Error &e)
+            {
+                std::cerr << "EXIF read failed: " << e.what() << std::endl;
+            }
+        }
+
+        // ================================
+        // Video format handling here
+        // ================================
+        if (extension == ".mov" || extension == ".mp4") {
+            using namespace MediaInfoLib;
+            MediaInfo MI;
+
+            MI.Open(make_mi_string(file_path.string()));
+
+            // it depends on different
+            String recordedDate = MI.Get(Stream_General, 0, __T("Recorded_Date"));
+            String encodedDate = MI.Get(Stream_General, 0, __T("Encoded_Date"));
+            String taggedDate = MI.Get(Stream_General, 0, __T("Tagged_Date"));
+            String date;
+            if (!recordedDate.empty())
+            {
+                date = recordedDate;
+            }
+            else if (!encodedDate.empty())
+            {
+                date = encodedDate;
+            }
+            else
+            {
+                date = taggedDate;
+            }
+            std::string date_part = make_std_string(date).substr(0, 10);
+            MI.Close();
+
+            fs::path target_dir =  current_dir / date_part;
+            fs::path target_file_path = target_dir / file_path.filename();
+
+            if (!fs::exists(target_dir))
+            {
+                fs::create_directory(target_dir);
+            }
+
+            move_to_file(file_path, target_file_path);
+        }
+    }
+
+    return 0;
+}
